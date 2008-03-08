1b532ea570a0bc490295962166a496a3c25d4746
Brian Cooke <bcooke@roobasoft.com>
2008-03-08 18:40:17 -0500

    Appropriately move things into the Sequel:: module

--- lib/sequel_mailer.rb
+++ lib/sequel_mailer.rb
@@ -10,46 +10,32 @@ if defined?(Merb::Plugins)
   end
   
   Merb::BootLoader.after_app_loads do
-    # require code that must be loaded before the application
     require "merb-mailer"
     require "merb_sequel"
     require "sequel"
     
-    class SequelMailer < Merb::Mailer      
-      @@email_class = Merb::Mailer.config[:sequel_mailer_class] || Email
+    module Sequel
+      class Mailer < Merb::Mailer
+        @@email_class = Merb::Mailer.config[:sequel_mailer_class] || Email rescue
   
-      ##
-      # Current email class for deliveries.
-      def self.email_class
-        @@email_class
-      end
-  
-      ##
-      # Sets the email class for deliveries.
-  
-      def self.email_class=(klass)
-        @@email_class = klass
-      end
-  
-      ##
-      # Adds +mail+ to the Email table.  Only the first From address for +mail+ is
-      # used.
-  
-      def sequel
-        debugger
-        @mail.to.each do |destination|
-          r = @@email_class.create({
-            :mail => @mail.to_s, 
-            :to_address => destination,
-            :from_address => @mail.from.first, 
-            :created_on => Time.now
-          })
+        ##
+        # Adds +mail+ to the Email table.  Only the first From address for +mail+ is
+        # used.
+        def sequel
+          @mail.to.each do |destination|
+            r = @@email_class.create({
+              :mail => @mail.to_s, 
+              :to_address => destination,
+              :from_address => @mail.from.first, 
+              :created_on => Time.now
+            })
+          end
         end
       end
-    end
     
-    class SequelMailController < Merb::MailController
-      self._mailer_klass = SequelMailer
+      class MailController < Merb::MailController
+        self._mailer_klass = Sequel::Mailer
+      end
     end
   end
   
